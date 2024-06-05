e591e3fe0516aaf8992dc3b38d84f7817dab32e5
Chen Zhenhuan <976949689@qq.com>
2024-06-06 00:26:00 +0800

    2024年6月6日00:25:56

--- completable-future/src/main/java/com/clearlove/_09_compare_price/ComparePriceDemo.java
+++ completable-future/src/main/java/com/clearlove/_09_compare_price/ComparePriceDemo.java
@@ -1,21 +1,57 @@
 package com.clearlove._09_compare_price;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author promise
  * @date 2024/6/5 - 23:24
  */
 public class ComparePriceDemo {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     ComparePriceService service = new ComparePriceService();
 
-    long start = System.currentTimeMillis();
-    PriceResult priceResult = service.getCheapestPlatformPrice("iPhone14");
-    long end = System.currentTimeMillis();
+    // 方案一测试
+    //    long start = System.currentTimeMillis();
+    //    PriceResult priceResult = service.getCheapestPlatformPrice("iPhone14");
+    //    long end = System.currentTimeMillis();
+    //
+    //    double costTime = (end - start) / 1000.0;
+    //    System.out.printf("cost %.2f second\n", costTime);
+    //
+    //    System.out.println("priceResult = " + priceResult);
 
-    double costTime = (end - start) / 1000.0;
-    System.out.printf("cost %.2f second\n", costTime);
+    // 方案二测试
+    //    long start = System.currentTimeMillis();
+    //    PriceResult priceResult = service.getCheapestPlatformPrice2("iPhone14");
+    //    long end = System.currentTimeMillis();
+    //
+    //    double costTime = (end - start) / 1000.0;
+    //    System.out.printf("cost %.2f second\n", costTime);
+    //
+    //    System.out.println("priceResult = " + priceResult);
 
+    // 方案三测试
+//    long start = System.currentTimeMillis();
+//    PriceResult priceResult = service.getCheapestPlatformPrice3("iPhone14");
+//    long end = System.currentTimeMillis();
+//
+//    double costTime = (end - start) / 1000.0;
+//    System.out.printf("cost %.2f second\n", costTime);
+//
+//    System.out.println("priceResult = " + priceResult);
+
+    /**
+     * 方案一：串行方式操作商品此价 costTime:6.09
+     * 方案二：Future+线程池提高了任务处理的并行性 costTime:2.09
+     * 方案三：使用CompletableFuture.进一步增强并行 costTime:1.10
+     */
+
+
+    // 测试在一个平台比较同款产品不同色系的价格
+    List<String> products = Arrays.asList("iPhone14黑色", "iPhone14白色", "iphone14玫瑰红");
+    PriceResult priceResult = service.batchComparePrice(products);
     System.out.println("priceResult = " + priceResult);
   }
 }
--- completable-future/src/main/java/com/clearlove/_09_compare_price/ComparePriceService.java
+++ completable-future/src/main/java/com/clearlove/_09_compare_price/ComparePriceService.java
@@ -2,6 +2,15 @@ package com.clearlove._09_compare_price;
 
 import com.clearlove.utils.CommonUtils;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -35,11 +44,110 @@ public class ComparePriceService {
         .get();
   }
 
+  // 方案二：使用Future+线程池增强并行
+  public PriceResult getCheapestPlatformPrice2(String productName) {
+    ExecutorService executorService = Executors.newFixedThreadPool(4);
+    // 获取淘宝平台的价格和优惠
+    Future<PriceResult> taobaoFuture =
+        executorService.submit(
+            () -> {
+              return computeRealPrice(
+                  HttpRequest.getTaoBaoPrice(productName),
+                  HttpRequest.getTaoBaoDiscount(productName));
+            });
+
+    // 获取京东平台的价格和优惠
+    Future<PriceResult> jingDongFuture =
+        executorService.submit(
+            () -> {
+              return computeRealPrice(
+                  HttpRequest.getJingDongPrice(productName),
+                  HttpRequest.getJingDongDiscount(productName));
+            });
+
+    // 获取拼多多平台的价格和优惠
+    Future<PriceResult> pDDFuture =
+        executorService.submit(
+            () -> {
+              return computeRealPrice(
+                  HttpRequest.getPDDPrice(productName), HttpRequest.getPDDDiscount(productName));
+            });
+
+    // 计算最优的平台和价格
+    return Stream.of(taobaoFuture, jingDongFuture, pDDFuture)
+        .map(
+            (future) -> {
+              try {
+                return future.get(5, TimeUnit.SECONDS);
+              } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+              } finally {
+                executorService.shutdown();
+              }
+            })
+        .filter(Objects::nonNull)
+        .min(Comparator.comparing(PriceResult::getRealPrice))
+        .get();
+  }
+
+  // 方案三：使用CompletableFuture进一步增强并行
+  public PriceResult getCheapestPlatformPrice3(String productName) {
+    // 获取淘宝平台的价格和优惠
+    CompletableFuture<PriceResult> taoBaoCF =
+        CompletableFuture.supplyAsync(() -> HttpRequest.getTaoBaoPrice(productName))
+            .thenCombine(
+                CompletableFuture.supplyAsync(() -> HttpRequest.getTaoBaoDiscount(productName)),
+                this::computeRealPrice);
+
+    // 获取京东平台的价格和优惠
+    CompletableFuture<PriceResult> jingDongCF =
+        CompletableFuture.supplyAsync(() -> HttpRequest.getJingDongPrice(productName))
+            .thenCombine(
+                CompletableFuture.supplyAsync(() -> HttpRequest.getJingDongDiscount(productName)),
+                this::computeRealPrice);
+
+    // 获取拼多多平台的价格和优惠
+    CompletableFuture<PriceResult> pDDCF =
+        CompletableFuture.supplyAsync(() -> HttpRequest.getPDDPrice(productName))
+            .thenCombine(
+                CompletableFuture.supplyAsync(() -> HttpRequest.getPDDDiscount(productName)),
+                this::computeRealPrice);
+
+    return Stream.of(taoBaoCF, jingDongCF, pDDCF)
+        .map(CompletableFuture::join)
+        .min(Comparator.comparing(PriceResult::getRealPrice))
+        .get();
+  }
+
   public PriceResult computeRealPrice(PriceResult priceResult, int discount) {
     priceResult.setRealPrice(priceResult.getPrice() - discount);
     priceResult.setDiscount(discount);
     CommonUtils.printThreadLog(
         priceResult.getPlatform() + "最终价格计算完成：" + priceResult.getRealPrice());
     return priceResult;
+  }
+
+  public PriceResult batchComparePrice(List<String> products) {
+    // step1:遍历每个商品的名字，根据商品名称开启异步任务获取最终价，归集到List集合中
+    List<CompletableFuture<PriceResult>> completableFutures =
+        products.stream()
+            .map(
+                productName -> {
+                  return CompletableFuture.supplyAsync(
+                          () -> HttpRequest.getTaoBaoPrice(productName))
+                      .thenCombine(
+                          CompletableFuture.supplyAsync(
+                              () -> HttpRequest.getTaoBaoDiscount(productName)),
+                          this::computeRealPrice);
+                })
+            .collect(Collectors.toList());
+
+    // step2:把多个商品的最终价进行排序获取最小值
+    return completableFutures.stream()
+        .map(CompletableFuture::join)
+        .sorted(Comparator.comparing(PriceResult::getRealPrice))
+        .findFirst()
+        .get();
   }
 }
